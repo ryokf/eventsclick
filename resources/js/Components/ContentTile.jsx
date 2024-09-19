e56a61fa79d06eb9875b4d274e3abf1e59bfe76d
ryokf <ryokhrisnaf@gmail.com>
2024-09-19 08:17:52 +0700

    ✨feature: carousel and program section

---
+++ resources/js/Components/ContentTile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ContentTile = () => {
+    return (
+        <div className="">
+            <div className="w-full aspect-video bg-gray-300 rounded-lg"></div>
+            <div className="mt-1">
+                <p className="text-xs text-primary">Category</p>
+                <h1 className="font-semibold text-sm">Lorem ipsum dolor sit</h1>
+                <p className="text-xs text-gray-400">15 September 2022</p>
+            </div>
+        </div>
+    )
+}
+
+export default ContentTile
--- resources/js/Pages/Media/Home.jsx
+++ resources/js/Pages/Media/Home.jsx
@@ -1,4 +1,5 @@
 import { Carousel } from '@/Components/Carousel'
+import ContentTile from '@/Components/ContentTile'
 import { Navbar } from '@/Components/Navbar'
 import React from 'react'
 
@@ -6,18 +7,29 @@ const Home = () => {
     return (
         <div className="">
             <Navbar></Navbar>
-            <div className="px-20 pt-6 h-svh">
-                {/* <Carousel></Carousel> */}
+            <div className="px-4 lg:px-20 pt-6 h-60 lg:h-svh">
+                <Carousel></Carousel>
             </div>
-            <ProgramSection></ProgramSection>
+            <div className="mx-4 lg:mx-20 my-10">
+                <ProgramSection></ProgramSection>
+            </div>
         </div>
     )
 }
 
-const ProgramSection = ()=> {
-    return(
+const ProgramSection = () => {
+    return (
         <div className="">
-            <h1 className="text-3xl font-medium">Program A</h1>
+            <div className="flex justify-between w-full">
+                <h1 className="text-2xl font-semibold mb-2">Program A</h1>
+            </div>
+            <div className="grid grid-cols-2 gap-2 w-full">
+                {
+                    [1, 2, 3, 4].map((item) => (
+                        <ContentTile />
+                    ))
+                }
+            </div>
         </div>
     )
 }
